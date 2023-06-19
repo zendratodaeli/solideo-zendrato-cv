019ae844ab6bcfcdf8890d69bf712d6230fe63ed
Solideo Putra Zendrato <solidzendrato@gmail.com>
2023-06-19 21:54:21 +0200

    set time out

--- javascript/main.js
+++ javascript/main.js
@@ -7,16 +7,27 @@ const navBarElement = document.querySelector('.nav-bar');
 const hamburgerElement = document.querySelector('.hamburger-menu');
 const buttonEmailMeBigScreen = document.querySelector('.email-me-button-big-screen');
 const buttonEmailMeMiniScreen = document.querySelector('.email-me-button-mini-screen');
+const imageElement = document.querySelector('.image-container');
+
 
 setTimeout(() => {
   nameElement.removeAttribute("hidden")
+  imageElement.removeAttribute("hidden");
   setTimeout(() => {
     descriptionElementH2.removeAttribute("hidden");
     setTimeout(() => {
       descriptionElementP.removeAttribute("hidden");
-    }, 1000)
-  }, 1000)
-}, 2*1000)
+      setTimeout(() => {
+        buttonEmailMeBigScreen.removeAttribute("hidden");
+        if (window.innerWidth <= 990) {
+          buttonEmailMeMiniScreen.style.display = 'block';
+        } else {
+          buttonEmailMeMiniScreen.style.display = 'none';
+        }
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 2*1000);
 
 
 var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
@@ -29,15 +40,11 @@ function removeElement() {
     spanLineRightElement.style.display = 'none';
     navBarElement.style.display = 'none';
     hamburgerElement.style.display = 'block';
-    // buttonEmailMeBigScreen.style.display = 'none';
-    // buttonEmailMeMiniScreen.style.display = 'block';
   } else {
     spanLineLeftElement.style.display = 'flex';
     spanLineRightElement.style.display = 'flex';
     navBarElement.style.display = 'flex';
     hamburgerElement.style.display = 'none';
-    // buttonEmailMeBigScreen.style.display = 'block';
-    // buttonEmailMeMiniScreen.style.display = 'none';
   }
 }
 
